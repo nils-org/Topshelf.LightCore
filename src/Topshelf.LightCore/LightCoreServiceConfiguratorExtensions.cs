04db16ee2fb9bcd1b826809b4720f9479f468483
Nils Andresen <nils@nils-andresen.de>
2020-10-10 16:05:47 +0200

    Merge branch 'main' into develop

--- src/Topshelf.LightCore/LightCoreServiceConfiguratorExtensions.cs
+++ src/Topshelf.LightCore/LightCoreServiceConfiguratorExtensions.cs
@@ -1,5 +1,7 @@
-﻿using LightCore;
-using System.Globalization;
+﻿using System.Globalization;
+
+using LightCore;
+
 using Topshelf.Logging;
 using Topshelf.ServiceConfigurators;
 
@@ -11,22 +13,24 @@ namespace Topshelf.LightCore
     public static class LightCoreServiceConfiguratorExtensions
     {
         /// <summary>
-        /// Construct a new service for a <see cref="ServiceConfigurator"/> using the pre-configured LightCore <see cref="IContainer"/>
+        /// Construct a new service for a <see cref="ServiceConfigurator"/> using the pre-configured LightCore <see cref="IContainer"/>.
         /// <seealso cref="HostConfiguratorExtension.UseLightCore(HostConfigurators.HostConfigurator, IContainer)"/>
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="configurator"></param>
-        /// <returns></returns>
-        public static ServiceConfigurator<T> ConstructUsingLightCore<T>(this ServiceConfigurator<T> configurator) where T : class
+        /// <typeparam name="T">TypeParam for the <see cref="ServiceConfigurator{T}"/>.</typeparam>
+        /// <param name="configurator">The <see cref="ServiceConfigurator{T}"/>to configure.</param>
+        /// <returns>the configured <see cref="ServiceConfigurator{T}"/>.</returns>
+        public static ServiceConfigurator<T> ConstructUsingLightCore<T>(this ServiceConfigurator<T> configurator)
+            where T : class
         {
             var log = HostLogger.Get(typeof(HostConfiguratorExtensions));
             log.Info(
-                string.Format(CultureInfo.InvariantCulture,
-                "[Topshelf.LightCore] Service configured to construct {0} using LightCore.", typeof(T).Name));
+                string.Format(
+                    CultureInfo.InvariantCulture,
+                    "[Topshelf.LightCore] Service configured to construct {0} using LightCore.",
+                    typeof(T).Name));
 
             configurator.ConstructUsing(serviceFactory => LightCoreBuilderConfigurator.Resolve<T>());
             return configurator;
         }
-
     }
 }
